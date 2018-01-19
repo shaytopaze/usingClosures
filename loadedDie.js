65f4fde82238ea6406a22f0e488f00b34aa57db9
Shay Alexander <shaytopazehair@gmail.com>
2018-01-19 01:36:03 +0000

    played with ID generator, and created loaded die function

---
+++ loadedDie.js
@@ -0,0 +1,17 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var i = -1;
+
+  return function() {
+
+    i ++;
+    return list[i];
+
+    }
+  }
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
